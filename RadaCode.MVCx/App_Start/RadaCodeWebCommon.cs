3835681844f0b20222814576f41d2fe8313df2ca
Katrin <ekaterina.omg@gmail.com>
2013-04-14 02:31:57 +0300

    Init commit

---
+++ RadaCode.MVCx/App_Start/RadaCodeWebCommon.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using RadaCode.MVCx.App_Start;
+using RadaCode.MVCx.Application;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(RadaCodeWebCommon), "Start")]
+
+namespace RadaCode.MVCx.App_Start
+{
+    public class RadaCodeWebCommon
+    {
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RadaCodeViewEngine());
+        }
+    }
+}
